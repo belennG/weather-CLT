8b154f5e4fbcd9e123b07996700215b6d27bce35
Maria Belen Gatti <mbelen@hotmail.com>
2021-04-02 08:44:03 +0000

    Add weatherEmojis fx

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import * as weather from "weather-js";
 import * as prompt from "prompt-promise";
+import * as emoji from "node-emoji";
 
 let getWeather = async function(){
     const query = await prompt("I'd like to know what the weather is like in.. ")
@@ -11,10 +12,29 @@ let getWeather = async function(){
     }
     weather.find({search: query, degreeType: 'C'}, function(err, result) {
         if(err) console.log(err);
-       
-        console.log(result[0].current.temperature, result[0].current.skytext);
+        const skyText = result[0].current.skytext;
+        let weatherEmojis = weatherEmoji(skyText);
+        //
+        console.log(result[0].current.temperature + skyText + weatherEmojis);
       })
-
+    
 } 
 
 getWeather();
+
+function weatherEmoji(skyText){
+    if(skyText == "Partly Sunny"){
+   return "It is not sunny!"
+    } if (skyText == "Mostly Cloudy"){
+
+    } if (skyText == "Cloudy"){
+
+    } if (skyText == "Sunny"){
+
+    } if (skyText == "Rainy"){
+
+    } if (skyText == "Snow"){
+
+}
+}
+
