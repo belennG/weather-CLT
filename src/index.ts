fd5f224add0bd177fc4e340bef90b945b747ab27
Maria Belen Gatti <mbelen@hotmail.com>
2021-04-02 09:30:35 +0000

    Add all the emojis

--- src/index.ts
+++ src/index.ts
@@ -11,30 +11,43 @@ let getWeather = async function(){
         degreeType: "C"
     }
     weather.find({search: query, degreeType: 'C'}, function(err, result) {
-        if(err) console.log(err);
+        if(err) console.log("Sorry, I could not find the place you are looking for");
         const skyText = result[0].current.skytext;
         let weatherEmojis = weatherEmoji(skyText);
         //
-        console.log(result[0].current.temperature + skyText + weatherEmojis);
+        console.log(result[0].current.temperature + " C° " + skyText + " " + weatherEmojis);
       })
+      getWeather();
     
 } 
 
 getWeather();
 
+
 function weatherEmoji(skyText){
     if(skyText == "Partly Sunny"){
-   return "It is not sunny!"
-    } if (skyText == "Mostly Cloudy"){
-
-    } if (skyText == "Cloudy"){
-
-    } if (skyText == "Sunny"){
-
-    } if (skyText == "Rainy"){
-
-    } if (skyText == "Snow"){
-
-}
-}
+        return emoji.get("barely_sunny")
+        } 
+    if (skyText == "Mostly Cloudy"){
+        return emoji.get("sun_behind_cloud")
+        } 
+    if (skyText == "Cloudy"){
+        return emoji.get("cloud");
+        } 
+    if (skyText == "Sunny"){
+        return emoji.get("sunny");
+        } 
+    if (skyText == "Rainy"){
+        return emoji.get("rain_cloud");
+        } 
+    if (skyText == "Snow"){
+        return emoji.get("snow_cloud")
+    }
+    if (skyText == "Mostly Clear"){
+        return emoji.get("sun_small_cloud")
+    }
+    if (skyText == "Clear"){
+        return emoji.get("full_moon")
+    }
+    }
 
