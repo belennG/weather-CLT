75926fd869b51253a417bfd3ab29aa3f46fe96b7
Maria Belen Gatti <mbelen@hotmail.com>
2021-06-11 12:47:29 +0000

    No clue

--- src/index.ts
+++ src/index.ts
@@ -3,80 +3,90 @@
 import * as weather from "weather-js";
 import * as prompt from "prompt-promise";
 import * as emoji from "node-emoji";
+const yourmama = require("yourmama");
 
-let getWeather = async function () {
-  const query = await prompt("I'd like to know what the weather is like in.. ");
+// let getWeather = async function () {
+//   const query = await prompt("I'd like to know what the weather is like in.. ");
 
-  weather.find({ search: query, degreeType: "C" }, function (err, results) {
-    if (err) {
-      console.log("Something went wrong", err);
-      getWeather();
-      return;
-    }
-    if (results.length == 0) {
-      console.log("Could not find what you are looking for :(");
+//   weather.find({ search: query, degreeType: "C" }, function (err, results) {
+//     if (err) {
+//       console.log("Something went wrong", err);
+//       getWeather();
+//       return;
+//     }
+//     if (results.length == 0) {
+//       console.log("Could not find what you are looking for :(");
 
-      return getWeather();
-    }
-    const skyText = results[0].current.skytext;
-    let weatherEmojis = weatherEmoji(skyText);
-    //
-    console.log(
-      results[0].current.temperature + " C° " + skyText + " " + weatherEmojis
-    );
-    if (results.length > 1) {
-      console.log(
-        "Now what you were looking for? Here are some other options: "
-      );
-      const arrayWithCities = results.map(function (result) {
-        return result.location.name;
-      });
-      arrayWithCities.shift();
-      console.log(arrayWithCities.join("\n"));
-    }
-    getWeather();
-  });
-};
+//       return getWeather();
+//     }
+//     const skyText = results[0].current.skytext;
+//     let weatherEmojis = weatherEmoji(skyText);
+//     //
+//     console.log(
+//       results[0].current.temperature + " C° " + skyText + " " + weatherEmojis
+//     );
+//     if (results.length > 1) {
+//       console.log(
+//         "Now what you were looking for? Here are some other options: "
+//       );
+//       const arrayWithCities = results.map(function (result) {
+//         return result.location.name;
+//       });
+//       arrayWithCities.shift();
+//       console.log(arrayWithCities.join("\n"));
+//     }
+//     getWeather();
+//   });
+// };
 
-getWeather();
+// getWeather();
 
-function weatherEmoji(skyText) {
-  if (skyText == "Partly Sunny") {
-    return emoji.get("barely_sunny");
-  }
-  if (skyText == "Mostly Cloudy") {
-    return emoji.get("sun_behind_cloud");
-  }
-  if (skyText == "Cloudy") {
-    return emoji.get("cloud");
-  }
-  if (skyText == "Sunny") {
-    return emoji.get("sunny");
-  }
-  if (skyText == "Rain") {
-    return emoji.get("rain_cloud");
-  }
-  if (skyText == "Snow") {
-    return emoji.get("snow_cloud");
-  }
-  if (skyText == "Mostly Clear") {
-    return emoji.get("sun_small_cloud");
-  }
-  if (skyText == "Clear") {
-    return emoji.get("full_moon");
-  }
-  if (skyText == "Mostly Sunny") {
-    return emoji.get("mostly_sunny");
-  }
-  if (skyText == "Partly Cloudy") {
-    return emoji.get("sun_behind_cloud");
-  }
-  if (skyText == "Rain Showers") {
-    return emoji.get("rain_cloud");
-  }
-  if (skyText == "Light Snow") {
-    return emoji.get("snow_cloud");
-  } else {
-    return emoji.get("snow_cloud");
-  }
-}
+// function weatherEmoji(skyText) {
+//   if (skyText == "Partly Sunny") {
+//     return emoji.get("barely_sunny");
+//   }
+//   if (skyText == "Mostly Cloudy") {
+//     return emoji.get("sun_behind_cloud");
+//   }
+//   if (skyText == "Cloudy") {
+//     return emoji.get("cloud");
+//   }
+//   if (skyText == "Sunny") {
+//     return emoji.get("sunny");
+//   }
+//   if (skyText == "Rain") {
+//     return emoji.get("rain_cloud");
+//   }
+//   if (skyText == "Snow") {
+//     return emoji.get("snow_cloud");
+//   }
+//   if (skyText == "Mostly Clear") {
+//     return emoji.get("sun_small_cloud");
+//   }
+//   if (skyText == "Clear") {
+//     return emoji.get("full_moon");
+//   }
+//   if (skyText == "Mostly Sunny") {
+//     return emoji.get("mostly_sunny");
+//   }
+//   if (skyText == "Partly Cloudy") {
+//     return emoji.get("sun_behind_cloud");
+//   }
+//   if (skyText == "Rain Showers") {
+//     return emoji.get("rain_cloud");
+//   }
+//   if (skyText == "Light Snow") {
+//     return emoji.get("snow_cloud");
+//   } else {
+//     return emoji.get("snow_cloud");
+//   }
+// }
+
+const randomJoke = yourmama.getRandom().then((roast) => {
+  return roast;
+});
+const fatJoke = yourmama.getTopic("fat").then((roast) => {
+  return roast;
+});
+
+randomJoke();
